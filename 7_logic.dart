417634375ce4e5c9318ae9eecab029fd62326ae6
Goran Šostarko <77921490+gsostarko@users.noreply.github.com>
2022-12-19 09:30:16 +0100

    Add files via upload

---
+++ 7_logic.dart
@@ -0,0 +1,16 @@
+void main() {
+  // Logic - If, If/Else, If Else If
+  var num = 51;
+  
+  // We can use > < >= <= !=
+  if (num == 5){
+    print("The number is $num");
+  } 
+  else if(num==3){
+    print("The number is 3");
+  }
+  
+  else{
+    print("The number is not $num");
+  }  
+}
