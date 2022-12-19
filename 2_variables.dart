b21f3e2461afdf95af5e9ff70a3ed60bebd53b96
Goran Šostarko <77921490+gsostarko@users.noreply.github.com>
2022-12-19 08:31:07 +0100

    Add files via upload

---
+++ 2_variables.dart
@@ -0,0 +1,30 @@
+void main() {
+  //Variables
+  
+  //Strings
+  //var name = "Goran";
+  String name = "Goran";
+  
+  //Integer
+  //var x = 41;
+  int x = 41;
+  
+  
+  //Dynamic
+  dynamic firstName = "Goran";
+  
+  
+  //Const and Final
+  const String fullName = "Goran Sostarko";
+  final String nickName = "G";
+  
+  
+  // devlaring and not using
+  var myName;
+  
+  print(myName);
+  
+  myName = "Goran";
+    
+  print(myName);
+}
